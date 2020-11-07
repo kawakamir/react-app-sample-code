146ca0857152f34091056851c407dc4dc69a79ee
kawakamir <rui.kawakami@gmail.com>
2020-11-07 13:17:54 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import * as ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router } from "react-router-dom";
 
-ReactDOM.render(<App></App>, document.getElementById("root"));
 import routes from "routes";
 
 const history = createBrowserHistory();
